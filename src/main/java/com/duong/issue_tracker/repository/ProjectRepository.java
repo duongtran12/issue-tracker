da4a4c4859f6a840b9c6126eb34ce8febe6c75fe
duongtran12 <tranducduong2204@gmail.com>
2026-09-25 09:51:03 +0700

    feat: query projects available to a member

--- src/main/java/com/duong/issue_tracker/repository/ProjectRepository.java
+++ src/main/java/com/duong/issue_tracker/repository/ProjectRepository.java
@@ -2,6 +2,8 @@ package com.duong.issue_tracker.repository;
 
 import com.duong.issue_tracker.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,16 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     boolean existsByKey(String key);
 
     List<Project> findAllByOwnerUsernameOrderByCreatedAtDesc(String username);
+
+    @Query("""
+            select p from Project p
+            where exists (
+                select pm.id from ProjectMember pm
+                where pm.project = p and pm.user.username = :username
+            )
+            order by p.createdAt desc
+            """)
+    List<Project> findAllAccessibleByUsername(@Param("username") String username);
 
     Optional<Project> findByIdAndOwnerUsername(Long id, String username);
 }
